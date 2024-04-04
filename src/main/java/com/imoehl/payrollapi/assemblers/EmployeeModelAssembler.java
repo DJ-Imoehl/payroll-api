74222f7848445657ec41ef1998c647ba6a39753c
DJ Imoehl <dj.imoehl@gmail.com>
2024-04-04 15:26:15 -0600

    Adding relational links

---
+++ src/main/java/com/imoehl/payrollapi/assemblers/EmployeeModelAssembler.java
@@ -0,0 +1,22 @@
+package com.imoehl.payrollapi.assemblers;
+
+import com.imoehl.payrollapi.controllers.EmployeeController;
+import com.imoehl.payrollapi.models.Employee;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).getEmployee(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).findAll()).withRel("employees"));
+    }
+
+}
--- src/main/java/com/imoehl/payrollapi/controllers/EmployeeController.java
+++ src/main/java/com/imoehl/payrollapi/controllers/EmployeeController.java
@@ -1,33 +1,52 @@
 package com.imoehl.payrollapi.controllers;
 
+import com.imoehl.payrollapi.assemblers.EmployeeModelAssembler;
 import com.imoehl.payrollapi.exceptions.EmployeeNotFoundException;
 import com.imoehl.payrollapi.models.Employee;
 import com.imoehl.payrollapi.repository.EmployeeRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.hateoas.EntityModel;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    EmployeeController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/employees")
-    List<Employee> findAll() {
-        return repository.findAll();
+    public CollectionModel<EntityModel<Employee>> findAll() {
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).findAll()).withSelfRel());
     }
 
     @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee) {
-        return repository.save(new Employee(newEmployee.getName(), newEmployee.getRole()));
+    ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) {
+        EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
     @GetMapping("/employees/{id}")
-    Employee getEmployee(@PathVariable Long id) {
-        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    public EntityModel<Employee> getEmployee(@PathVariable Long id) {
+        Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
